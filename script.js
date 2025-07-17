b7b0ed3a32f7178af67d09275702e04bab4b7fa3
MOA2300 <seleneluna0199@gmail.com>
2025-07-17 14:52:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -106,8 +106,8 @@ window.onload = () => {
     // Adjust these based on your image scale/size
     if (isOpen) {
       flipTrigger.style.left = "30px";
-      flipTrigger.style.top = "20px";
-      flipTrigger.style.width = "180px";
+      flipTrigger.style.top = "40px";
+      flipTrigger.style.width = "125px";
       flipTrigger.style.height = "90px";
     } else {
       flipTrigger.style.left = "120px";
